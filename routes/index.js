3bee7b17e0dd841399832014c7865cf49104f3ac
Alokkumarshah <alokandmessi@gmail.com>
2025-10-18 15:34:59 +0530

    Your commit message

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get("/", (req, res) => {
   res.render("pages/home", { title: "SDC Games | Home" });
 });
 
+// Let's Begin route
+router.get("/lets-begin", (req, res) => {
+  res.render("pages/lets-begin", { title: "SDC Games | Let's Begin" });
+});
+
+// Participants route
+router.get("/participants", (req, res) => {
+  res.render("pages/participants", { title: "SDC Games | Participants" });
+});
+
+// Game Hosts route
+router.get("/game-hosts", (req, res) => {
+  res.render("pages/game-hosts", { title: "SDC Games | Game Hosts" });
+});
+
 export default router;
