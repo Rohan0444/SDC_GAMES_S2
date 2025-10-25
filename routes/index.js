bb378823274134c52ef3da02b6458296815d973b
Rohan0444 <rawatrohan443@gmail.com>
2025-10-26 00:05:36 +0530

    Final

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,23 @@ const router = express.Router();
 
 // Home route
 router.get("/", (req, res) => {
-  res.render("pages/home", { title: "SDC Games | Home" });
+  res.render("pages/home-2", { title: "SDC Games | Home" });
 });
+
+// // New Home route (Winners page)
+// router.get("/home-2", (req, res) => {
+//   res.render("pages/home-2", { title: "SDC Games | Winners" });
+// });
 
 // Let's Begin route
 router.get("/lets-begin", (req, res) => {
-  res.render("pages/lets-begin", { title: "SDC Games | Let's Begin" });
+  res.render("pages/lets-begin-2", { title: "SDC Games | Let's Begin" });
 });
+
+// Game Over route
+// router.get("/lets-begin-2", (req, res) => {
+//   res.render("pages/lets-begin-2", { title: "SDC Games | Game Over" });
+// });
 
 // Participants route
 router.get("/participants", (req, res) => {
